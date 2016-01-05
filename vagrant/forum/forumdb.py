10320b4b4efea544a092056bd5f002e16b9bdcf9
Will Orzo <stevejobsss3000@gmail.com>
2016-01-05 17:01:12 -0500

    update AddPost function to inoculate against injec

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,7 +1,7 @@
 #
 # Database access functions for the web forum.
 
-
+import psycopg2
 import time
 
 ## Database connection
@@ -21,9 +21,9 @@ def GetAllPosts():
     '''
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.exectue("SELECT time, content FROM posts ORDER BY time DESC")
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
     posts = ({'content': str(row[1]), 'time': str(row[0])}
-             for row in c.fectchall())
+             for row in c.fetchall())
     DB.close()
     return posts
 
@@ -34,5 +34,8 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("INSERT INTO posts (content) VALUES (%s);", (content,))
+    DB.commit()
+    DB.close()
